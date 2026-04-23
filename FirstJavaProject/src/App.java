6eaf1fd88687c149baae220a735e6f6ae2fd8382
Luigi Gandemer <luigigandemer6@gmail.com>
2026-04-23 14:58:23 +0200

    First Java Program

--- FirstJavaProject/src/App.java
+++ FirstJavaProject/src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int num1 = 10;
+        int num2 = 20;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
     }
 }
